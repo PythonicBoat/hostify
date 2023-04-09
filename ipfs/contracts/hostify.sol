ee050b282bdd7cc6b55cc6fb97d8d55b16531abe
Yashvardhan Singh <68675629+PythonicBoat@users.noreply.github.com>
2023-04-09 11:57:57 +0530

    Update hostify.sol

--- ipfs/contracts/hostify.sol
+++ ipfs/contracts/hostify.sol
@@ -1,27 +1,93 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import IPFS library
-import "./IPFS.sol";
+contract Hostify {
+    // Name
+    string public name = "Hostify";
 
-contract CDN {
-    // Initialize IPFS object
-    IPFS private ipfs;
+    // Number of files
+    uint256 public fileCount = 0;
 
-    // Set the IPFS object in the constructor
-    constructor(address ipfsAddress) {
-        ipfs = IPFS(ipfsAddress);
+    // Mapping fileId=>Struct
+    mapping(uint256 => File) public files;
+
+    // Struct
+    struct File {
+        uint256 fileId;
+        string fileHash;
+        uint256 fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint256 uploadTime;
+        address payable uploader;
     }
 
-    // Add file to IPFS and return the file hash
-    function addFile(bytes memory fileData) public returns (bytes32) {
-        bytes32 hash = ipfs.add(fileData);
-        return hash;
-    }
+    // Event ( Oracle )
+    event FileUploaded(
+        uint256 fileId,
+        string fileHash,
+        uint256 fileSize,
+        string fileType,
+        string fileName,
+        string fileDescription,
+        uint256 uploadTime,
+        address payable uploader
+    );
 
-    // Retrieve file from IPFS by its hash
-    function getFile(bytes32 hash) public view returns (bytes memory) {
-        bytes memory fileData = ipfs.get(hash);
-        return fileData;
+    constructor() public {}
+
+    // Upload File function
+    function uploadFile(
+        string memory _fileHash,
+        uint256 _fileSize,
+        string memory _fileType,
+        string memory _fileName,
+        string memory _fileDescription
+    ) public {
+        // Make sure the file hash exists
+        require(bytes(_fileHash).length > 0);
+
+        // Make sure file type exists
+        require(bytes(_fileType).length > 0);
+
+        // Make sure file description exists
+        require(bytes(_fileDescription).length > 0);
+
+        // Make sure file fileName exists
+        require(bytes(_fileName).length > 0);
+
+        // Make sure uploader address exists
+        require(msg.sender != address(0));
+
+        // Make sure file size is more than 0
+        require(_fileSize > 0);
+
+        // Increment file id
+        fileCount++;
+
+        // Add File to the contract
+        files[fileCount] = File(
+            fileCount,
+            _fileHash,
+            _fileSize,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            block.timestamp,
+            msg.sender
+        );
+
+        // Trigger an event
+        emit FileUploaded(
+            fileCount,
+            _fileHash,
+            _fileSize,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            now,
+            msg.sender
+        );
     }
 }
