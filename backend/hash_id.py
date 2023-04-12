56ded654d43759e4ecfd268582aac1042de7e9d1
Pythonicboat <>
2023-04-12 20:06:24 +0000

    added app routes

--- backend/hash_id.py
+++ backend/hash_id.py
@@ -1,1 +1,9 @@
-# Function
+# Function to convert string to hash
+import hashlib
+
+# user ID would follow different format than products IDs
+
+def sha256_hash(string):
+    return hashlib.sha256(string.encode()).hexdigest()
+
+# print(sha256_hash("Hello World")) --> a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e
