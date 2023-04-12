dbe3145e0259d2b51f5daff7ac91d97586e0f053
Pythonicboat <vs21yash@gmail.com>
2023-04-12 19:33:36 +0530

    change

--- ipfs/src/ipfs.js
+++
@@ -1,6 +0,0 @@
-const IPFS = require('ipfs-http-client');
-
-// Connect to IPFS daemon API server
-const ipfs = new IPFS({ host : 'localhost' , port : '3000' , protocol : 'http' });
-
-export default ipfs;
--- ipfs/src/web3.js
+++
@@ -1,5 +0,0 @@
-import Web3 from './web3';
-
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:3000'));
-
-export default web3;
